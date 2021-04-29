6c209fb0dcd79db548ec275ace8f6b00834dab6e
anshuli-chitade <chitadeanshuli@gmail.com>
2021-04-29 23:07:45 +0530

    ADDED file problem_3.js

---
+++ problem_3.js
@@ -0,0 +1,11 @@
+
+ var result;
+ var year = prompt("enter an year");
+ if(year%4 ==0 ){
+    console.log( +year+ " is a leap year");
+ }
+ else{
+    console.log( +year+ " is not a leap year");
+ }
+    
+    
