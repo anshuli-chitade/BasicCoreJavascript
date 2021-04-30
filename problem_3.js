a08c27537700d9f8f413218a65e78cf37d45dbf3
anshuli-chitade <chitadeanshuli@gmail.com>
2021-05-01 00:11:55 +0530

    MODIFIED conditions in problem_3

--- problem_3.js
+++ problem_3.js
@@ -1,9 +1,12 @@
 
  var result;
  var year = prompt("enter an year");
- if(year%4 ==0 ){
-    console.log( +year+ " is a leap year");
- }
+ if(len<4){
+   console.log(" an year should be 4 digit")
+}
+else if(year%4==0){
+  console.log( +year+ " is a leap year")
+}
  else{
     console.log( +year+ " is not a leap year");
  }
