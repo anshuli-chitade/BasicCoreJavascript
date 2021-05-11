2eb6e633b41986e9f65b4ad7da9241cdca20da1f
anshuli-chitade <chitadeanshuli@gmail.com>
2021-05-12 00:16:25 +0530

    REFACTORED function and applied conditions according to the problem statement

--- windChill.js
+++ windChill.js
@@ -1,10 +1,18 @@
 
+const read=require("readline-sync");
 const calculate = (temp, wSpeed) => {
-    const windChill = Math.round((35.74 + (0.6215 * temp)) + (((0.4275 * temp) - 35.75) * Math.pow(wSpeed, 0.16)));
-    console.log("Wind Chill:" + windChill);
+   
+    if(temp<50 && wSpeed<120 && wSpeed>3){    
+        const windChill = Math.round((35.74 + (0.6215 * temp)) + (((0.4275 * temp) - 35.75) * Math.pow(wSpeed, 0.16)));
+        console.log("Wind Chill:" + windChill);    
+    }
+    else{
+        console.log("please check temp should be less than 50 and speed should be less than 120 and greater than 3")
+    }  
 }
-
-const temp = prompt("Enter the temperature");
-const wSpeed = prompt("Enter the wind speed");
-calculate = (temp, wSpeed);
+console.log("Enter the temperature")
+const temp = read.questionInt();
+console.log("Enter the wind speed")
+const wSpeed = read.questionInt();
+calculate(temp, wSpeed);
 
